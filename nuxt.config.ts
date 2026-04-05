427eee1699a4ec6896b0df61791207ecca67bc3f
luojiahai <ljiahai@hotmail.com>
2026-04-06 08:25:54 +1000

    chore: add vite plugin to ignore sourcemap warnings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,24 @@ export default defineNuxtConfig({
     ],
     detectBrowserLanguage: false,
   },
+  vite: {
+    plugins: [
+    {
+        name: "vite-plugin-ignore-sourcemap-warnings",
+        apply: "build",
+        configResolved(config) {
+          config.build.rollupOptions.onwarn = (warning, warn) => {
+            if (
+              warning.code === "SOURCEMAP_BROKEN" &&
+              warning.plugin === "@tailwindcss/vite:generate:build"
+            ) {
+              return;
+            }
+
+            warn(warning);
+          };
+        },
+      },
+    ],
+  }
 });
